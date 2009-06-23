37292ed8744f2dc5ff52cb033385ca1650e1499d
Adam Wiggins <adam@heroku.com>
2009-06-22 23:59:49 -0700

    REDIS_URL to point at redis server

--- lib/all.rb
+++ lib/all.rb
@@ -3,7 +3,16 @@ require 'ostruct'
 require 'active_support/core_ext/time'
 
 require 'redis'
-DB = Redis.new
+
+redis_config = if ENV['REDIS_URL']
+	require 'uri'
+	uri = URI.parse ENV['REDIS_URL']
+	{ :host => uri.host, :port => uri.port, :password => uri.password, :db => uri.path.gsub(/^\//, '') }
+else
+	{}
+end
+
+DB = Redis.new(redis_config)
 
 require File.dirname(__FILE__) + '/../vendor/maruku/maruku'
 
